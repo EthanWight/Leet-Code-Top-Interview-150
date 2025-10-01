f5addc84df78a7b627dc4f1f1389dc371bf94e3a
Ethan Wight <ethanwight26@gmail.com>
2025-09-30 21:31:16 -0400

    Add solution for "Best Time to Buy and Sell Stock II" problem with test cases

--- src/Array_String/Best_Time_To_Buy_And_Sell_Stock_V2.java
+++ src/Array_String/Best_Time_To_Buy_And_Sell_Stock_V2.java
@@ -2,9 +2,11 @@
 122. Best Time to Buy and Sell Stock II
 Medium
 
-You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+You are given an integer array price where prices[i] is the price of a given stock on the ith day.
 
-On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold than one share of the stock.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the
+stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring
+you never hold more than one share of the stock.
 
 Find and return the maximum profit you can achieve.
 
@@ -27,7 +29,8 @@ Example 3:
 
 Input: prices = [7,6,4,3,1]
 Output: 0
-Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the
+maximum profit of 0.
 
 
 Constraints:
